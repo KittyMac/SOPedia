3ed597ade5c642ed73fe02e4a430f2b37153a2e1
Rocco Bowling <rocco@chimerasw.com>
2020-11-20 15:45:38 -0500

    adding the other space monsters

--- Sources/SOPediaPamphlet/empires.json.swift
+++ Sources/SOPediaPamphlet/empires.json.swift
@@ -5,7 +5,7 @@ import Foundation
 public extension SOPediaPamphlet {
     static func EmpiresJson() -> String {
 return ###"""
-{"empires":[{"uuid":"amoeba","title":"Amoeba","hasRace":true,"teamType":"monster","AcquiredTechnology":["shipHullFrigate","shipHullDestroyer","shipHullCruiser","shipHullBattleship","shipHullTitan","shipHullMammoth","laserBeam1","heavyArmorPlating1"]}]}
+{"empires":[{"uuid":"amoeba","title":"Amoeba","hasRace":true,"teamType":"monster","acquiredTechnologys":["shipHullFrigate","shipHullDestroyer","shipHullCruiser","shipHullBattleship","laserBeam1","heavyArmorPlating1"]},{"uuid":"serpent","title":"Serpent","hasRace":true,"teamType":"monster","acquiredTechnologys":["shipHullFrigate","shipHullDestroyer","shipHullCruiser","shipHullBattleship","gaussTurret1",],"raceAttributes":["combatPilots"]},{"uuid":"crystal","title":"Crystal","hasRace":true,"teamType":"monster","acquiredTechnologys":["shipHullFrigate","shipHullDestroyer","shipHullCruiser","shipHullBattleship","ionPulseCannon1",],"raceAttributes":["combatPilots","fantasticEngineers"]},{"uuid":"energy","title":"Strange Energy","hasRace":true,"teamType":"monster","acquiredTechnologys":["shipHullFrigate","shipHullDestroyer","shipHullCruiser","shipHullBattleship","gaussTurret3",]}]}
 """###
     }
 }
@@ -17,4 +17,4 @@ public extension SOPediaPamphlet {
     }
 }
 
-private let gzip_data = Data(base64Encoded:"H4sIAAAAAAACA1XOPQvCQAwG4P+S2cW1W6uIiyDSTTrENvQO7qPmcsJR+t9NhUId8+RNeGcgP1mmBNVzhpztABWgj/RCOIBYcaRQb2AwPbBXEs6ke0LflmmN+BiSEGum7t9ZPw4t9SZEF8eivyEZO12zcxe2IwppbpMzJeFYfrebnTjb9CcNinZZpx22VjDs5ht6H8WoONTzRusd19aEn1Kzj3x3KDaMR+iWbvkCrCcACfwAAAA=")
+private let gzip_data = Data(base64Encoded:"H4sIAAAAAAACA82RsW6EMAyGX6XKzHLqdhtHr+qIemwVg+HcECk41HEqoRPvXnMSCLaOjPn82/EnPwz2g2OM5vz1MCm5uzkb6AM2YDIjTjwqyBfQQfyEVpFwQq0j9NU4zJE+UBRkzUD7k3TivcK2o+CDHefhJnZu+Ejev7OzIKjBhbxhFA7js3lhBScXd+QCosvML4UetHjR30/zUgi/Y8594NKDOLInU0/ZaqPJAUk2OreVHMXHQoqxSswoalRnhnWrXIRdk+R5HNOGvgEpnQ8Sd34tj1HAb/yKlRzFzwUqk49YAFGgfylm5htIIIprr2QdIfLeGwnZjtuzCgNZfLkuhQNe91XVp3r6A1gWtcd3AwAA")
