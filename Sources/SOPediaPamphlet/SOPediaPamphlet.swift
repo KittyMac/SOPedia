2ab81ac9c3ae3e85b8289d647afd18ec11520bff
Rocco Bowling <rocco@chimerasw.com>
2020-12-09 23:16:20 -0500

    adding markdown for unexplored star system displays

--- Sources/SOPediaPamphlet/SOPediaPamphlet.swift
+++ Sources/SOPediaPamphlet/SOPediaPamphlet.swift
@@ -6,6 +6,7 @@ public enum SOPediaPamphlet {
     public static func get(string member: String) -> String? {
         switch member {
         case "/empires.json": return SOPediaPamphlet.EmpiresJson()
+        case "/en.json": return SOPediaPamphlet.EnJson()
         case "/infrastructure.json": return SOPediaPamphlet.InfrastructureJson()
         case "/races.json": return SOPediaPamphlet.RacesJson()
         case "/technology.json": return SOPediaPamphlet.TechnologyJson()
@@ -16,6 +17,7 @@ public enum SOPediaPamphlet {
     public static func get(gzip member: String) -> Data? {
         switch member {
         case "/empires.json": return SOPediaPamphlet.EmpiresJsonGzip()
+        case "/en.json": return SOPediaPamphlet.EnJsonGzip()
         case "/infrastructure.json": return SOPediaPamphlet.InfrastructureJsonGzip()
         case "/races.json": return SOPediaPamphlet.RacesJsonGzip()
         case "/technology.json": return SOPediaPamphlet.TechnologyJsonGzip()
