92d323944b3cb8dbffb590153f2677b6bdf661d1
Rocco Bowling <rocco@chimerasw.com>
2021-03-25 17:20:19 -0400

    fix for cluster script

--- meta/cluster.sh
+++ meta/cluster.sh
@@ -23,7 +23,7 @@ then
         htop
 
     if [[ -f "/usr/share/keyrings/docker-archive-keyring.gpg" ]]; then
-        
+        echo "docker gpg file already exists"
     else
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     fi
