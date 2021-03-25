b583b665eb5f24b8f0857be8848bea546281a632
Rocco Bowling <rocco@chimerasw.com>
2021-03-25 15:24:30 -0400

    consolidating the pi and odroid setup script into one

--- meta/cluster.sh
+++ meta/cluster.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-S="    "
+SWARM_TOKEN="$1"
+MANAGER_IP_ADDRESS="$2"
 
-echo default-on > /sys/class/leds/blue\:heartbeat/trigger
+S="    "
+ARCH=`uname -m`
 
 read -p 'Enter 192.168.1.XXX: ' N
 
-read -p "Initialize node as odroid${N} @ 192.168.1.${N}. Are you sure? " -n 1 -r
+read -p "Initialize node as pi${N} @ 192.168.1.${N}. Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo default-on > /sys/class/leds/blue\:heartbeat/trigger
-    
+then    
     # install docker
     sudo apt-get -q remove -y docker docker-engine docker.io containerd runc
     sudo apt-get -q update -y
@@ -23,14 +23,26 @@ then
         htop
 
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    if [[ "$ARCH" == "aarch64" ]]; then
+        echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    else
+        echo "deb [arch=armhf signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    fi
 
     sudo apt-get -q update -y
     sudo apt-get -q install -y docker-ce docker-ce-cli containerd.io
 
     # set the hostname
-    sed -i "s/odroid[0-9]*/odroid${N}/g" /etc/hostname
-    sed -i "s/odroid[0-9]*/odroid${N}/g" /etc/hosts
+    sed -i "s/pi[0-9]*/pi${N}/g" /etc/hostname
+    sed -i "s/pi[0-9]*/pi${N}/g" /etc/hosts
+    
+    # join the docker swarm
+    if [[ "$SWARM_TOKEN" == "" ]]; then
+        echo "No swarm token provided, unable to join a docker swarm"
+    else
+        sudo docker swarm join --token "$SWARM_TOKEN" "$MANAGER_IP_ADDRESS"
+    fi
 
     # set static IP address
     printf "network:\n${S}ethernets:\n${S}${S}eth0:\n${S}${S}${S}dhcp4: no\n${S}${S}${S}addresses:\n${S}${S}${S} - 192.168.1.${N}/24\n${S}${S}${S}gateway4: 192.168.1.254\n${S}${S}${S}nameservers:\n${S}${S}${S}${S}addresses: [8.8.8.8, 1.1.1.1]\n${S}version: 2\n" > /etc/netplan/50-cloud-init.yaml
