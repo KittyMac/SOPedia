4950947c1458f71af79226372fa221029f1ccb09
Rocco Bowling <rocco@chimerasw.com>
2022-02-03 19:13:08 -0500

    install docker's docker

--- meta/cluster.fedora.sh
+++ meta/cluster.fedora.sh
@@ -34,13 +34,25 @@ then
     sudo cpupower frequency-set -g performance
     
     # disable SELinux
-    sudo sed -i -e 's/SELINUX=/SELINUX=disabled #/g' /etc/selinux/config
+    #sudo sed -i -e 's/SELINUX=/SELINUX=disabled #/g' /etc/selinux/config
             
-    # install docker (Fedora way)
-    # https://fedoramagazine.org/docker-and-fedora-35/
-    sudo dnf -y install moby-engine docker-compose
-    sudo sed -i 's/--live-restore//g' /etc/sysconfig/docker
-    sudo sed -i 's/--selinux-enabled//g' /etc/sysconfig/docker
+    # install docker
+    # https://docs.docker.com/engine/install/fedora/
+    sudo dnf -y remove docker \
+              docker-client \
+              docker-client-latest \
+              docker-common \
+              docker-latest \
+              docker-latest-logrotate \
+              docker-logrotate \
+              docker-selinux \
+              docker-engine-selinux \
+              docker-engine
+    
+    sudo dnf -y install dnf-plugins-core
+    sudo dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf -y install docker-ce docker-ce-cli containerd.io
+    
     sudo systemctl enable docker
     sudo systemctl start docker
     
