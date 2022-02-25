9055c4e8b45419dc842697f11987115b177f0dbe
Rocco Bowling <rocco@chimerasw.com>
2022-02-25 13:07:43 -0500

    latest fedora cluster script

--- meta/cluster.fedora.sh
+++ meta/cluster.fedora.sh
@@ -32,10 +32,7 @@ then
     sudo ipmitool sensor thres FAN6 lower 100 150 250
 
     sudo cpupower frequency-set -g performance
-    
-    # disable SELinux
-    #sudo sed -i -e 's/SELINUX=/SELINUX=disabled #/g' /etc/selinux/config
-            
+                
     # install docker
     # https://docs.docker.com/engine/install/fedora/
     sudo dnf -y remove docker \
@@ -76,7 +73,7 @@ then
     sudo nmcli general hostname cluster${N}
     
     sudo nmcli con mod "Wired connection 1" ipv4.addresses "192.168.1.${N}/24" ipv4.gateway "192.168.1.254" ipv4.dns "8.8.8.8,1.1.1.1" ipv4.dns-search "attlocal.net" ipv4.method "manual"
-    #nmcli con mod "Wired connection 1" ipv4.addresses "" ipv4.gateway "" ipv4.dns "" ipv4.dns-search "" ipv4.method "auto"
+    #sudo nmcli con mod "Wired connection 1" ipv4.addresses "" ipv4.gateway "" ipv4.dns "" ipv4.dns-search "" ipv4.method "auto"
 
     echo "Finished!"
     
