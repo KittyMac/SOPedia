782a56eafbf2b1a830a60f704d06449788c5f64f
Rocco Bowling <rocco@chimerasw.com>
2022-04-02 18:49:51 -0400

    unvendored pamphlet

--- meta/CombinedBuildPhases.sh
+++ meta/CombinedBuildPhases.sh
@@ -9,6 +9,6 @@ if [ "$(uname)" == "Darwin" ]; then
     set -e
 
     # Pamphlet - Generates swift code which embeds resources in our server executable
-    ./pamphlet --prefix SOPedia --release ./Resources/ ./Sources/SOPediaPamphlet/ 
+    pamphlet --prefix SOPedia --release ./Resources/ ./Sources/SOPediaPamphlet/ 
 
 fi
