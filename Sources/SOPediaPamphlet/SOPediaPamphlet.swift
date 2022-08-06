8dbe7d28987dacb9554fc26a4ad96a9e34bbd6d1
Rocco Bowling <rocco@chimerasw.com>
2022-08-06 16:45:05 -0400

    updated build artifacts

--- Sources/SOPediaPamphlet/SOPediaPamphlet.swift
+++ Sources/SOPediaPamphlet/SOPediaPamphlet.swift
@@ -5,7 +5,7 @@ import Foundation
 
 
 public enum SOPediaPamphlet {
-    public static let version = "v0.1.1-13-g821fdfc"
+    public static let version = "v0.1.1-14-g3882556"
 
     public static func get(string member: String) -> StaticString? {
         switch member {
