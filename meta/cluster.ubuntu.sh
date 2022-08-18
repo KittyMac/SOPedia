0665508a0d04a3af7b4e9eae809b772de52ae611
Rocco Bowling <rocco@chimerasw.com>
2022-08-18 19:43:41 -0400

    us docker install script

--- meta/cluster.ubuntu.sh
+++ meta/cluster.ubuntu.sh
@@ -12,25 +12,14 @@ read -p "Initialize node as cluster${N} @ 192.168.1.${N}. Are you sure? " -n 1 -
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # install docker
-    sudo apt-get -q remove docker docker-engine docker.io containerd runc
     sudo apt-get -q update -y
     sudo apt-get -q install -y \
-        ca-certificates \
         curl \
-        gnupg \
-        lsb-release \
         htop \
         network-manager
     
-    sudo mkdir -p /etc/apt/keyrings
-    rm -f /etc/apt/keyrings/docker.gpg
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
     
     # join the docker swarm
     if [[ "$SWARM_TOKEN" == "" ]]; then
@@ -40,7 +29,6 @@ then
     fi
 
     # (new) method uses the NetworkManager
-    
     echo $'[main]\nplugins=ifupdown,keyfile\n\n[ifupdown]\nmanaged=true\n\n[keyfile]\nunmanaged-devices=none\n\n[device]\nwifi.scan-rand-mac-address=no' | sudo tee /etc/NetworkManager/NetworkManager.conf
     
     sudo nmcli general hostname cluster${N}
